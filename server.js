3f93a0eb60cb2da64f9127c661b1ea0e57ced3de
TovarishFin <tovarishfin@gmail.com>
2017-09-12 05:43:09 +0200

    updated server

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, './dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', function (request, response){
-    response.sendFile(path.resolve(__dirname, './dist', 'index.html'));
+    response.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 app.listen(process.env.PORT || 8080);
