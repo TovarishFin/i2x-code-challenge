624d0062a5e35c9389cf8c92a564790f522e34ee
TovarishFin <tovarishfin@gmail.com>
2017-09-12 05:30:20 +0200

    proc

--- server.js
+++ server.js
@@ -2,7 +2,4 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname + '/src/'));
-app.get('/', function(res, res) {
-  res.sendFile(__dirname + '/dist/index.html');
-})
 app.listen(process.env.PORT || 8080);
