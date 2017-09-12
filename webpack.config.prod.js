384f9f953e8ffefd032170d9b2a3572a0553641b
TovarishFin <tovarishfin@gmail.com>
2017-09-12 05:08:31 +0200

    server added

---
+++ webpack.config.prod.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname + '/dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'react-hot-loader!babel-loader'
+      },
+      {
+        test: /\.css$/, loader: "style-loader!css-loader"
+      }
+    ]
+  },
+}
