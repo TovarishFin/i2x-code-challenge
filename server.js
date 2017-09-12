3b9e0994c574305dd15ee98a3e445bc8f6643d88
TovarishFin <tovarishfin@gmail.com>
2017-09-13 01:47:31 +0200

    fixed server file due to new buld folder name

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.static(__dirname + 'app'));
 
 app.get('*', function(req, res){
-  res.sendFile(__dirname + 'app/index.html');
+  res.sendFile(__dirname + '/app/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
